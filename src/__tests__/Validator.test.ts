0b19dfed3d48f685287ce3a774f3a6089db73b7a
mitsuYashi <hm20011027@gmail.com>
2023-06-05 22:35:50 +0900

    feat: メールアドレスのテスト

--- src/__tests__/Validator.test.ts
+++ src/__tests__/Validator.test.ts
@@ -2,6 +2,25 @@ import { Validator } from "../index";
 
 describe("Validatorクラス", () => {
   it("Errorオブジェクトと一致する", () => {
-    expect(Validator).toEqual(new Error("Not Implemented"));
+    expect(Validator.email).toEqual(new Error("Not Implemented"));
+  });
+});
+
+describe("メールアドレス", () => {
+  it("通常", () => {
+    const value = "aaa@example.com";
+    const tmp = Validator.email(value);
+    expect(tmp).toEqual(value);
+  });
+  it(".を二個含む", () => {
+    const value = "bbb@sample.co.jp";
+    const tmp = Validator.email(value);
+    expect(tmp).toEqual(value);
+  });
+
+  it("@を含まない", () => {
+    const value = "ccc.example.com";
+    const tmp = Validator.email(value);
+    expect(tmp).toEqual(new Error("Not Matched"));
   });
 });
