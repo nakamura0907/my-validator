76e775ee16cbdcf2920f1fbf2669a23ccf56bead
nakamura0907 <nakamura990907@gmail.com>
2023-06-09 20:59:27 +0900

    仮のサンプルテストケース作成 #3

--- src/__tests__/Validator.test.ts
+++ src/__tests__/Validator.test.ts
@@ -1,7 +1,33 @@
-import { Validator } from "../index";
+describe("validatorオブジェクト", () => {
+  it("正規表現チェック", () => {
+    const schema = validator.regex(/^[0-9]+$/);
 
-describe("Validatorクラス", () => {
-  it("Errorオブジェクトと一致する", () => {
-    expect(Validator).toEqual(new Error("Not Implemented"));
+    expect(schema.validate(123)).toEqual(123);
+  });
+  it("複数のチェックを行う", () => {
+    const schema = validator
+      .make()
+      .required()
+      .min(3)
+      .build();
+    
+    expect(schema.validate("aaa")).toEqual("aaa");
   });
 });
+
+describe("Schemaクラス", () => {
+  const schema = validator.regex(/^[0-9]+$/);
+
+  const validValue = 123;
+  const invalidValue = "abc";
+
+  it("値または例外を返す", () => {
+    expect(schema.validate(validValue)).toEqual(validValue);
+    expect(() => schema.validate(invalidValue)).toThrow(ValidationError);
+  });
+
+  it("Resultクラスを返す", () => {
+    expect(schema.validate(validValue)).toBeInstanceOf(Result);
+    expect(schema.validate(invalidValue)).toBeInstanceOf(Result);
+  });
+})
